21ce7dec6e68943f0231d3d835e6034ef5ca6678
Tho Nguyen <tho.nguyen@transwap.com>
2023-03-01 22:41:54 +0700

    implement app role by attribute

---
+++ Demo.Api/Config/AppRolesConvention.cs
@@ -0,0 +1,29 @@
+﻿using Demo.Api.Attributes;
+using Demo.Api.Extensions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Linq;
+
+namespace Demo.Api.Config
+{
+	public class AppRolesConvention : IControllerModelConvention
+	{
+		private readonly AppRoles _appRoles;
+
+        public AppRolesConvention(AppRoles appRoles)
+        {
+            _appRoles = appRoles;
+        }
+        public void Apply(ControllerModel controller)
+		{
+			var actions = controller.Actions.Where(a => !a.Attributes.Any(attr => attr.GetType() == typeof(AppRoleAttribute) && ((AppRoleAttribute)attr).AppRoles.Contains(_appRoles))).ToList();
+			controller.Actions.RemoveRange(actions);
+		}
+	}
+
+	public enum AppRoles
+	{
+		App = 1,
+		IntegrationTest = 2,
+		OtherApp = 3
+	}
+}
---
+++ Demo.Api/Extensions/CollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Demo.Api.Extensions
+{
+	public static class CollectionExtension
+	{
+		public static void RemoveRange<T>(this ICollection<T> @this, IEnumerable<T> collection)
+		{
+
+			if (@this == null || collection == null)
+			{
+				return;
+			}
+
+			if (@this.IsReadOnly)
+			{
+				throw new NotSupportedException("The read-only collection can't remove items.");
+			}
+
+			foreach (T item in collection)
+			{
+				@this.Remove(item);
+			}
+		}
+	}
+}
