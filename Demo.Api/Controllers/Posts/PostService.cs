dbcee976dca0aba6a13cb0727243b8dc2641fe5a
Tho Nguyen <tho.nguyen@transwap.com>
2023-03-10 14:37:54 +0700

    implement option

---
+++ Demo.Api/Controllers/Posts/PostService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Demo.Api.Controllers.Posts
+{
+	public interface IPostService
+	{
+		PostOptions GetPostOptions();
+	}
+	public class PostService: IPostService
+	{
+		public readonly PostOptions _options;
+        public PostService(IOptions<PostOptions> options)
+        {
+            _options = options.Value;
+        }
+		public PostOptions GetPostOptions() => _options;
+	}
+
+	public class PostOptions
+	{
+		public TypeInfo Type { get; set; }
+		public NumberInfo Number { get; set; }
+        public PostOptions()
+        {
+			Type = new TypeInfo();
+			Number = new NumberInfo();
+        }
+    }
+
+	public class TypeInfo
+	{
+        public TypeInfo()
+        {
+			Type1 = "Type1";
+			Type2 = "Type2";
+        }
+        public string Type1 { get; set; }
+		public string Type2 { get; set; }
+	}
+	public class NumberInfo
+	{
+		public int Num1 { get; set; }
+		public int Num2 { get; set; }
+        public NumberInfo()
+        {
+			Num1 = 1;
+			Num2 = 2;
+        }
+    }
+}
---
+++ Demo.Api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Api.Controllers.Posts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo.Api.Extensions
+{
+	public static class ServiceCollectionExtension
+	{
+		public static IServiceCollection AddPostOption(this IServiceCollection services) // IConfiguration configuration)
+		{
+			var postOption = new PostOptions();
+			
+			services.AddScoped<IPostService, PostService>();
+			// services.Configure<PostOptions>(configuration.GetSection("PostOptions"));
+			services.Configure<PostOptions>(option => 
+			{
+				option.Number = postOption.Number;
+				option.Type = postOption.Type;
+			});
+
+			return services;
+		}
+	}
+}
